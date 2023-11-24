74825609ea91cd8a34dc572d4f3d00a31801864f
Sayori-h <3186385448@qq.com>
2023-11-24 17:07:40 +0800

    完成日志模块是实现： 完成LogInfo类

--- HPlayerServer/HPlayerServer/Logger.cpp
+++ HPlayerServer/HPlayerServer/Logger.cpp
@@ -1,7 +1,7 @@
 #include "Logger.h"
 
-CLoggerServer::CLoggerServer():
-	m_thread(&CLoggerServer::ThreadFunc,this)
+CLoggerServer::CLoggerServer() :
+	m_thread(&CLoggerServer::ThreadFunc, this)
 {
 	m_server = nullptr;
 	m_path = "./log/" + GetTimeStr() + ".log";
@@ -21,7 +21,7 @@ int CLoggerServer::Start()
 {
 	if (m_server != nullptr)return -1;
 	if (access("log", W_OK | R_OK) != 0)
-		mkdir("log", S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);	
+		mkdir("log", S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	m_file = fopen(m_path, "w+");
 	if (m_file == NULL)return -2;
 	int ret = m_epoll.Create(1);
@@ -48,7 +48,7 @@ int CLoggerServer::ThreadFunc()
 {
 	std::vector<epoll_event> events;
 	std::map<int, CSocketBase*> mapClients;
-	while (m_thread.isVaild()&& (m_epoll != -1) && (m_server != nullptr)) {
+	while (m_thread.isVaild() && (m_epoll != -1) && (m_server != nullptr)) {
 		ssize_t ret = m_epoll.WaitEvents(events, 1);
 		if (ret < 0)break;
 		if (ret > 0) {
@@ -88,8 +88,8 @@ int CLoggerServer::ThreadFunc()
 			if (i != ret)break;
 		}
 	}
-	for (auto it = mapClients.begin(); it !=mapClients.end(); it++) {
-		if (it->second) delete it->second;			
+	for (auto it = mapClients.begin(); it != mapClients.end(); it++) {
+		if (it->second) delete it->second;
 	}
 	mapClients.clear();
 	return 0;
@@ -131,10 +131,10 @@ Buffer CLoggerServer::GetTimeStr()
 	timeb tmb;
 	ftime(&tmb);
 	tm* pTm = localtime(&tmb.time);
-	int nSize = snprintf(result,result.size(),
-		"%04d-%02d-%02d+%02d-%02d-%02d+%03d",
-		pTm->tm_year + 1900, pTm->tm_mon +1, pTm->tm_mday,
-		pTm->tm_hour, pTm->tm_min, pTm -> tm_sec,tmb.millitm);
+	int nSize = snprintf(result, result.size(),
+		"%04d-%02d-%02d %02d-%02d-%02d %03d",
+		pTm->tm_year + 1900, pTm->tm_mon + 1, pTm->tm_mday,
+		pTm->tm_hour, pTm->tm_min, pTm->tm_sec, tmb.millitm);
 	result.resize(nSize);
 	return result;
 }
@@ -151,4 +151,102 @@ void CLoggerServer::WriteLog(const Buffer& data)
 		fflush(pFile);
 #endif // DEBUG 
 	}
+}
+
+LogInfo::LogInfo(const char* file, int line, const char* func, pid_t pid,
+	pthread_t tid, int logType, const char* format, ...)
+{
+	const char sLogType[][8] = { "INFO","DEBUG","WARNING","ERROR","FATAL" };
+	char* buf = nullptr;
+	m_bIsStreamLog = false;
+	int count = asprintf(&buf, "%s(%d):[%s][%s]<%d-%d>(%s)\n", file, line, sLogType[logType],
+		(char*)CLoggerServer::GetTimeStr(), pid, tid, func);
+	if (count > 0) {
+		m_buf = buf;
+		free(buf);
+	}
+	else return;
+
+	va_list ap;
+	va_start(ap, format);
+	count = vasprintf(&buf, format, ap);
+	if (count > 0) {
+		m_buf += buf;
+		free(buf);
+	}
+	va_end(ap);
+}
+
+LogInfo::LogInfo(const char* file, int line, const char* func,
+	pid_t pid, pthread_t tid, int logType)
+{
+	char* buf = nullptr;
+	m_bIsStreamLog = true;//自己主动发送的 流式日志	
+	const char sLogType[][8] = { "INFO","DEBUG","WARNING","ERROR","FATAL" };
+	int count = asprintf(&buf, "%s(%d):[%s][%s]<%d-%d>(%s)\n", file, line, sLogType[logType],
+		(char*)CLoggerServer::GetTimeStr(), pid, tid, func);
+	if (count > 0) {
+		m_buf = buf;
+		free(buf);
+	}
+}
+/*测试用例：
+const char* message = "Hello, Logging!";
+LogInfo log("example.cpp", 42, "main", getpid(), pthread_self(), LOG_DEBUG, message, strlen(message));
+
+example.cpp(42):[DEBUG][时间]<进程ID-线程ID>(main)
+48 65 6C 6C 6F 2C 20 4C 6F 67 67 69 6E 67 21 00    ; Hello, Logging!.
+
+核心是通过 snprintf 将每个字节以两位的十六进制格式追加到字符串 m_buf 中。在每行的末尾，将可读的字符表示追加到字符串中。
+如果数据总长度不是16的倍数，最后一行会进行特殊处理，确保对齐和显示。
+*/
+LogInfo::LogInfo(const char* file, int line, const char* func,
+	pid_t pid, pthread_t tid, int logType, void* pData, size_t nSize)
+{
+	char* buf = nullptr;
+	m_bIsStreamLog = false;
+	const char sLogType[][8] = { "INFO","DEBUG","WARNING","ERROR","FATAL" };
+	int count = asprintf(&buf, "%s(%d):[%s][%s]<%d-%d>(%s)\n", file, line, sLogType[logType],
+		(char*)CLoggerServer::GetTimeStr(), pid, tid, func);
+	if (count > 0) {
+		m_buf = buf;
+		free(buf);
+	}
+	else return;
+
+	size_t i = 0;
+	char* Data = (char*)pData;
+	for (; i < nSize; i++) {
+		char buf[16] = "";
+		snprintf(buf, sizeof(buf), "%02X ", Data[i] & 0xFF);
+		m_buf += buf;
+		if (0 == ((i + 1) % 16)) {
+			m_buf += "\t; ";
+			for (size_t j = i - 15; j <= i; j++) {
+				if ((Data[j] & 0xFF) > 31 && ((Data[j] & 0xFF) < 0x7f))
+					m_buf += Data[i];
+				else m_buf += '.';
+			}
+			m_buf += '\n';
+		}
+
+		//处理最后一行
+		size_t k = i % 16;
+		if (k != 0) {
+			for (size_t j = 0; j < 16 - k; j++) m_buf += "   ";
+			m_buf += "\t;";
+			for (size_t j = i - k; j <= i; j++) {
+				if ((Data[i] & 0xFF) > 31 && ((Data[j] & 0xFF) < 0x7F))
+					m_buf += Data[i];
+				else m_buf += '.';
+			}
+			m_buf += "\n";
+		}
+	}
+}
+
+LogInfo::~LogInfo()
+{
+	if (m_bIsStreamLog)
+		CLoggerServer::Trace(*this);
 }
--- HPlayerServer/HPlayerServer/Logger.h
+++ HPlayerServer/HPlayerServer/Logger.h
@@ -4,16 +4,40 @@
 #include "Socket.h"
 #include "Debug.h"
 #include <sys/timeb.h>
+#include <stdarg.h>
+#include <sstream>
 #include <sys/stat.h>
+
+enum LogLevel {
+	LOG_INFO,
+	LOG_DEBUG,
+	LOG_WARNING,
+	LOG_ERROR,
+	LOG_FATAL
+};
 
 class LogInfo
 {
 public:
-	LogInfo();
+	LogInfo(const char* file, int line, const char* func, pid_t pid,
+		pthread_t tid,int logType,const char*format,...);
+	LogInfo(const char* file, int line, const char* func, pid_t pid,
+		pthread_t tid, int logType);
+	LogInfo(const char* file, int line, const char* func, pid_t pid,
+		pthread_t tid, int logType,void*pData,size_t nSize);
 	~LogInfo();
 	operator Buffer()const { return m_buf; }
+	template<typename T>
+	LogInfo& operator<<(const T& data) {
+		std::stringstream stream;
+		stream << data;
+		m_buf += stream.str();
+		return *this;
+	}
 private:
 	Buffer m_buf;
+	//默认是false 流式日志，则为true
+	bool m_bIsStreamLog;
 };
 
 class CLoggerServer
@@ -40,4 +64,37 @@ private:
 	Buffer m_path;
 	FILE* m_file;
 };
+
+#ifndef TRACE
+#define TRACE_INFO(...) CLoggerServer::Trace(LogInfo(__FILE__,__LINE__,\
+__FUNCTION__,getpid(),pthread_self(),LOG_INFO,__VA_ARGS__))
+#define TRACE_DEBUG(...) CLoggerServer::Trace(LogInfo(__FILE__,__LINE__,\
+__FUNCTION__,getpid(),pthread_self(),LOG_DEBUG,__VA_ARGS__))
+#define TRACE_WARNING(...) CLoggerServer::Trace(LogInfo(__FILE__,__LINE__,\
+__FUNCTION__,getpid(),pthread_self(),LOG_WARNING,__VA_ARGS__))
+#define TRACE_ERROR(...) CLoggerServer::Trace(LogInfo(__FILE__,__LINE__,\
+__FUNCTION__,getpid(),pthread_self(),LOG_ERROR,__VA_ARGS__))
+#define TRACE_FATAL(...) CLoggerServer::Trace(LogInfo(__FILE__,__LINE__,\
+__FUNCTION__,getpid(),pthread_self(),LOG_FATAL,__VA_ARGS__))
+
+//LOG_INFO<<"hello"<<"how are you";只有流式日志是自己主动发的
+#define LOG_INFO LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),pthread_self(),LOG_INFO)
+#define LOG_DEBUG LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),pthread_self(),LOG_DEBUG)
+#define LOG_WARNING LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),pthread_self(),LOG_WARNING)
+#define LOG_ERROR LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),pthread_self(),LOG_ERROR)
+#define LOG_FATAL LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),pthread_self(),LOG_FATAL)
+
+//内存导出
+//00 01 02 65…… ; ...a……
+#define DUMP_INFO(data,size) LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),\
+pthread_self(),LOG_INFO,data,size)
+#define DUMP_DEBUG(data,size) LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),\
+pthread_self(),LOG_DEBUG,data,size)
+#define DUMP_WARNING(data,size) LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),\
+pthread_self(),LOG_WARNING,data,size)
+#define DUMP_ERROR(data,size) LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),\
+pthread_self(),LOG_ERROR,data,size)
+#define DUMP_FATAL(data,size) LogInfo(__FILE__,__LINE__,__FUNCTION__,getpid(),\
+pthread_self(),LOG_FATAL,data,size)
+#endif
 
